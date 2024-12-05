c10240ff620a4c169452e234271a3c644d58625d
enomoto <naoya-enomoto@sanei-sys.com>
2024-12-05 17:02:32 +0900

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hello every one!'));
+app.get('/', (req, res) => res.send('Hello every morning project challenge success!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
